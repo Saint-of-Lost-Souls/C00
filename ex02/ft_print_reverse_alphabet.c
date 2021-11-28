b88f7cbb820346c84cda78efa33b66d457093a1a
Shade St Jude <shade.st.jude84@gmail.com>
2021-11-28 19:06:11 +1030

    Init upload

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void    ft_print_alphabet(void)
+{
+    char letter;
+    letter = 'z';
+
+    while (letter > 'a')
+    {
+       write(1, &letter, 1);
+       letter--;
+    } 
+    write(1, "\n", 1);
+
+}
+int main()
+{
+    ft_print_alphabet();
+    return 0;    
+}
---
+++ ex03/ft_print_numbers.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void    ft_print_numbers(void)
+{
+    char num;
+    num = 49;
+
+    while (num < 58)
+    {
+        write(1, &num, 1);
+        num++;
+    } 
+    write(1, "\n", 1);
+
+}
+int main()
+{
+    ft_print_numbers();
+    return 0;    
+}
