8e1809a6eff76e66d2674759b8c74e37b6f5ed76
Shade St Jude <92859483+Saint-of-Lost-Souls@users.noreply.github.com>
2021-11-29 16:40:46 +1030

    Update maff_revalpha.c

--- Random exercises/maff_revalpha.c
+++ Random exercises/maff_revalpha.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 #include <ctype.h>
-#include <stdio.h>
 
 void    ft_maff_revalpha()
 {   
@@ -22,23 +21,6 @@ void    ft_maff_revalpha()
         count++;                
     }
 }
- 
-/* // counter for the loop
-    int i = 0;
-
-    // word to convert to uppercase
-    char word[] = "edUcaTivE\n"; 
-    char chr; 
-
-    // Loop
-    while (word[i]) { 
-        chr = word[i]; 
-        printf("%c", toupper(chr)); 
-        i++; 
-    } 
-  
-    return 0; 
-}  */
 
 int main()
 {
